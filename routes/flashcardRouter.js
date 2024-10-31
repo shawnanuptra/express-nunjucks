5960e8af10073d0f3bea7aa975fc45d2f6f74817
Shawn <shawn.martin@sunderlandsoftwarecity.com>
2024-10-31 11:32:09 +0000

    removed flashcard and startgame controller

--- routes/flashcardRouter.js
+++ routes/flashcardRouter.js
@@ -1,13 +1,75 @@
 import express from "express";
-import flashcardController from "../controllers/flashcardController.js";
-import { renderStartGame, submitAnswer } from "../controllers/startGameController.js";
 const router = express.Router();
 
 // gets
-router.get("/", flashcardController);
-router.get("/start", renderStartGame);
+router.get("/", (req, res) => {
+	const flashcard = req.app.locals.flashcards.find(
+		(flashcard) => flashcard.slug === req.query.slug,
+	);
+	res.render("flashcard.njk", {
+		name: flashcard.name,
+		slug: flashcard.slug,
+		questions: flashcard.questions,
+	});
+});
+router.get("/start", (req, res) => {
+	const flashcard = req.app.locals.flashcards.find(
+		(flashcard) => flashcard.slug === req.query.slug,
+	);
+
+	const questionIndex = res.app.locals.questionIndex ?? 0;
+
+	res.render("start.njk", { question: flashcard.questions[questionIndex].q });
+});
 
 // post
-router.post("/start", submitAnswer);
+router.post("/start", (req, res) => {
+	const userAnswer = req.body.answer;
+	const flashcard = req.app.locals.flashcards.find(
+		(flashcard) => flashcard.slug === req.query.slug,
+	);
+
+	const totalQuestions = flashcard.questions.length;
+	const questionIndex = res.app.locals.questionIndex ?? 0;
+	const answer = flashcard.questions[questionIndex].a;
+
+	res.app.locals.questionIndex = questionIndex + 1;
+
+	const finished = res.app.locals.questionIndex >= totalQuestions;
+
+	// check answer
+	if (answer === userAnswer) {
+		req.app.locals.correctAnswersCount++;
+		const correctAnswersCount = req.app.locals.correctAnswersCount;
+		// show correct state
+		res.render("answer.njk", {
+			slug: flashcard.slug,
+			correctAnswer: answer,
+			userAnswer,
+			correct: true,
+			finished,
+			totalQuestions,
+			correctAnswersCount,
+		});
+	} else {
+		const correctAnswersCount = req.app.locals.correctAnswersCount;
+		// show wrong state
+		res.render("answer.njk", {
+			slug: flashcard.slug,
+			correctAnswer: answer,
+			userAnswer,
+			correct: false,
+			finished,
+			totalQuestions,
+			correctAnswersCount,
+		});
+	}
+
+	// reset questionIndex
+	if (finished) {
+		res.app.locals.questionIndex = 0;
+		res.app.locals.correctAnswersCount = 0;
+	}
+});
 
 export default router;
